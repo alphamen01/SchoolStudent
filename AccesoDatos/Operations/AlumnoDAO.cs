145a7b70279725b63c1172cb63dc911ac6edd600
alphamen01 <lesg.2018@hotmail.com>
2023-08-17 18:00:18 -0500

    se implemento la seleccion de alumnos e insercion con EntityFrameworkCore

---
+++ AccesoDatos/Operations/AlumnoDAO.cs
@@ -0,0 +1,66 @@
+﻿using AccesoDatos.Context;
+using AccesoDatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesoDatos.Operations
+{
+    public class AlumnoDAO
+    {
+        public ProyectoContext context = new ProyectoContext();
+
+        public List<Alumno> seleccionarTodos()
+        {
+            var alumnos = context.Alumnos.ToList<Alumno>();
+            return alumnos;
+        }
+
+        public Alumno seleccionar(int id)
+        {
+            var alumno = context.Alumnos.FirstOrDefault(c => c.Id == id);
+            return alumno!;
+        }
+
+        ////public bool insertar(Alumno alumno)
+        ////{
+        ////    try
+        ////    {
+        ////        context.Alumnos.Add(alumno);
+        ////        context.SaveChanges();
+        ////        return true;
+        ////    }
+        ////    catch(Exception ex)
+        ////    {
+        ////        return false;
+        ////    }
+
+        ////}
+        
+        public bool insertar(string dni,string nombre, string direccion,int edad, string email)
+        {
+            try
+            {
+                Alumno alumno = new Alumno();
+                alumno.Dni = dni;
+                alumno.Nombre = nombre;
+                alumno.Direccion = direccion;
+                alumno.Edad = edad;
+                alumno.Email = email;
+
+                context.Alumnos.Add(alumno);
+                context.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+    }
+}
+
+    
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using AccesoDatos.Models;
+using AccesoDatos.Operations;
+using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {      
+        AlumnoDAO opAlumno = new AlumnoDAO();
+
+        Console.WriteLine("*******************************************");
+        opAlumno.insertar("88811", "Luger", "Gramadal", 55, "luger@gmail.com");
+
+        var alumnos = opAlumno.seleccionarTodos();
+
+        foreach (var item in alumnos)
+        {
+            Console.WriteLine(item.Dni);
+        }
+
+        Console.WriteLine("-----------------------------------");
+
+        /*Console.WriteLine("Ingrese el id:");
+        var id = int.Parse(Console.ReadLine()!);
+        var alumno = opAlumno.seleccionar(id);
+
+        Console.WriteLine(alumno.Nombre);*/
+        /*
+        ////Alumno alumno = new Alumno()
+        ////{
+        ////    Nombre = "Blanca",
+        ////    Dni = "00000000",
+        ////    Direccion = "Los  liris",
+        ////    Edad = 66,
+        ////    Email = "lesg.22.com"
+        ////};
+        //--//Alumno alumno = new Alumno();
+        //--//alumno.Nombre = "Luis";
+        //--//alumno.Dni = "88811188";
+        //--//alumno.Direccion = "Los pinos";
+        //--//alumno.Edad = 26;
+        //--//alumno.Email = "lesg.com";
+
+        ////opAlumno.insertar(alumno!);
+        */
+        Console.WriteLine("*******************************************");
+
+    }
+}
